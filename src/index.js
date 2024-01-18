cefd05e1b439c4c5dadbbe3c8357d7b5dc3187e2
Dawson <13419524+dawson68@user.noreply.gitee.com>
2024-01-18 19:05:45 +0800

    finish the create component of Layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import router from "./router";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ src/pages/Layout/index.js
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+const Layout = () => {
+  return (
+    <div className="layout">
+      <div className="layout__content">Content goes here</div>
+    </div>
+  );
+};
+
+export default Layout;
